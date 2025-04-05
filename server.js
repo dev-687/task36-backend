47d76e1921334e0521a16912ba57dd6a3e412be0
dev soft <dev.soft687@gmail.com>
2025-04-05 17:53:37 +0530

    cors origin correction

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://task36-frontend.vercel.app",
   methods: ["GET", "POST", "DELETE"],
   credentials: true
 }));
