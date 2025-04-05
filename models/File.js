43e913f1f2322556694b448cb55a6e1522eddae8
dev soft <dev.soft687@gmail.com>
2025-04-05 18:51:32 +0530

    remove user

--- models/File.js
+++ models/File.js
@@ -1,23 +1,24 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const FileSchema = new mongoose.Schema(
-    {
-        filename: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        filePath: {
-            type: String,
-            required: true,
-        },
-        public_id: {
-            type: String,
-            required: true,
-        },
-        
+  {
+    filename: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    { timestamps: true }
+    filePath: {
+      type: String,
+      required: true,
+    },
+    public_id: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model('File', FileSchema);
+const File = mongoose.model('File', FileSchema);
+
+export default File;
